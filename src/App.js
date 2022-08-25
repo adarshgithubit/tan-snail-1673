4f0c2feead3470b2794b4e3d1eed9d9e7b353cef
adarshgithubit <104212058+adarshgithubit@users.noreply.github.com>
2022-08-25 23:34:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,49 +4,29 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <div class="mobile_navbar">
-      <div class="nav_container">
-        <div class="ham_menu">
-          <div></div>
-          <div></div>
-        </div>
-        <a href="./index.html"
-          ><img
-            src="https://www.rodanandfields.com/en-us/medias/rf-logo.svg?context=bWFzdGVyfGltYWdlc3w3MzczfGltYWdlL3N2Zyt4bWx8aW1hZ2VzL2g2MC9oYWIvODgyMjE0NzE4NjcxOC5zdmd8YjNmMmU2YTg5MTM0NTMzM2Y2ODg2ZmRkZTJhNmY2OWZhYmYyYjk5NWQxODkxODFiYjVkY2MxY2NjOWRlMzA5OA"
-            alt="Rodan + Fields"
-        /></a>
-        <div class="links">
-          <div class="search"><i class="bx bx-search"></i></div>
-          <div class="cart"><i class="bx bx-shopping-bag"></i></div>
-        </div>
-      </div>
-    </div>
-
-    <div class="mobile_menu">
-      <div class="menu_container">
-        <div class="nav_top">
-          <div class="sign_in">Sign in <i class="bx bx-user"></i></div>
-          <div>X</div>
-        </div>
-        <div>Shop By Category</div>
-        <div>Shop By Concern</div>
-        <div>Shop By Product Line</div>
-        <div>Best Sellers</div>
-        <div>Featured</div>
-        <div>Our Story</div>
-        <div>Perks Program</div>
-        <div>Become A Consultant</div>
-        <div>Blog</div>
-        <div>Find A Consultant</div>
-        <div class="country">
-          <img
-            src="https://www.rodanandfields.com/en-us/_ui/dest/images/flagUSA.png"
-            alt="Flag"
-          />
-          <span>English</span>
+      <div class="cart_area">
+        <div class="cart_box">
+          <div class="cart_nav">
+            <i class="bx bx-shopping-bag"></i>
+            <h3>Your Bag</h3>
+            <p>X</p>
+          </div>
+          <div class="cart_mid">
+            <h3>Your Bag Is Empty</h3>
+            <p>Shop now to add products to your bag</p>
+            <button class="cart_btn">START SHOPPING</button>
+          </div>
+          <div class="cart_items"></div>
+          <div class="cart_footer">
+            <p>Savings, shipping costs and tax calculated at checkout</p>
+            <div class="price_total">
+              <h3>SUBTOTAL</h3>
+              <h3></h3>
+            </div>
+            <buttom class="cart_btn">Review Bag & Checkout</buttom>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
